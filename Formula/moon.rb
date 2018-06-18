5a66dc2fd40a6dbad18aaeafa5331f964c24edbc
Paul Carleton <paulcarletonjr@gmail.com>
2018-06-17 20:06:24 -0700

    Update version to include caching

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -3,8 +3,8 @@
 class Moon < Formula
   desc "A CLI utility for information about the moon"
   homepage ""
-  url "https://github.com/pcarleton/moon/archive/v0.1.0.tar.gz"
-  sha256 "a8cf0bad62a7a83936e548a0168f6838c38139d9daf1afbe204616f41d495c3d"
+  url "https://github.com/pcarleton/moon/archive/v0.1.1.tar.gz"
+  sha256 "8dd71353a7b608790700ffe65f83b500f02d9666d6a8478a8cf2830fc1c95731"
   depends_on "rust" => :build
 
   def install
