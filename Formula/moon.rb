7c523f0a4ff67cffa9430bfb304a9b0a0abdf915
Paul Carleton <paulcarletonjr@gmail.com>
2018-06-20 10:45:35 -0700

    Update moon version to handle optional parameters

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -3,8 +3,8 @@
 class Moon < Formula
   desc "A CLI utility for information about the moon"
   homepage ""
-  url "https://github.com/pcarleton/moon/archive/v0.1.1.tar.gz"
-  sha256 "8dd71353a7b608790700ffe65f83b500f02d9666d6a8478a8cf2830fc1c95731"
+  url "https://github.com/pcarleton/moon/archive/v0.1.2.tar.gz"
+  sha256 "674c2547db20f112605b31a14f1da06805e6d2d0b255b8a1ab3ac825ebf8837f"
   depends_on "rust" => :build
 
   def install
