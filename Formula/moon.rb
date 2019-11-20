af03d95456250aa277e833a9e6130f8bbef70f37
Paul Carleton <paulcarletonjr@gmail.com>
2019-11-20 13:14:17 -0600

    Bump release to offline calculation verson v0.2.0

--- Formula/moon.rb
+++ Formula/moon.rb
@@ -3,8 +3,8 @@
 class Moon < Formula
   desc "A CLI utility for information about the moon"
   homepage ""
-  url "https://github.com/pcarleton/moon/archive/v0.1.2.tar.gz"
-  sha256 "674c2547db20f112605b31a14f1da06805e6d2d0b255b8a1ab3ac825ebf8837f"
+  url "https://github.com/pcarleton/moon/archive/v0.2.0.tar.gz"
+  sha256 "ffeb7c5d24db6db2acfa691f4be38bb75529ad88b816b9d61bf600a5d07e8d04"
   depends_on "rust" => :build
 
   def install
